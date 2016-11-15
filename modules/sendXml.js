c414977de72eec630b52a9a4970f91a8d7afb5cb
baoxiaoying <791736879@qq.com>
2016-11-15 21:31:50 +0800

    tij

---
+++ modules/sendXml.js
@@ -0,0 +1,12 @@
+var xml2js = require('xml2js'),
+		builder = new xml2js.Builder();
+modules.exports = function(res, data, callBack){
+	if(!data || !Object.keys(data).length){
+		res.send('success');
+	}else{
+		data.FromUserName = 'B-XY9301'
+		let xml = builder.buildObject({'xml': data});
+		res.send(xml);
+	}
+	if(callBack) callBack();
+};
--- routes/weixin.js
+++ routes/weixin.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+var sendXml = require('../modules/sendXml.js');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send(req.query.echostr);
 });
 router.post('/', function(req, res, next) {
 	console.log(req.bodyXml);
-	res.send('success');
+	var json = {};
+	json.ToUserName = req.bodyXml.FromUserName;
+	json.CreateTime = new Date().getTime();
+	json.MsgType = 'text';
+	json.Content = '你是';
+	console.log(json);
+	sendXml(res,json)
 });
 
 module.exports = router;
