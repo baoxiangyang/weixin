a34fca295fa06981473ae4545343785f4f2e8724
baoxiaoying <791736879@qq.com>
2016-11-15 21:59:48 +0800

    tjsdf

--- modules/sendXml.js
+++ modules/sendXml.js
@@ -6,9 +6,9 @@ module.exports = function(res, data, callBack){
 	}else{
 		data.FromUserName = 'B-XY9301'
 		let xml = builder.buildObject({'xml': data});
-		let str = '<xml><ToUserName>oZz9bw6wDYpBlZUde8gqHa--tWoU</ToUserName>'+
+		let str = '<xml><ToUserName><![CDATA[oZz9bw6wDYpBlZUde8gqHa--tWoU]]</ToUserName>'+
 '<CreateTime>'+new Date().getTime()+'</CreateTime>'+
-'<MsgType>text</MsgType><Content>sbsbs</Content><FromUserName>B-XY9301</FromUserName></xml>';
+'<MsgType><![CDATA[text]]</MsgType><Content><![CDATA[sbsbs]]</Content><FromUserName><![CDATA[B-XY9301]]</FromUserName></xml>';
 		res.send(str);
 	}
 	if(callBack) callBack();
