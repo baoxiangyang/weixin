edb4cc38608ee8ae52490508c5603e04fd77e46a
baozi <xybao@qq.com>
2016-11-16 23:13:37 +0800

    tijao

--- modules/myRequest.js
+++ modules/myRequest.js
@@ -2,7 +2,7 @@
 var request = require('request');
 module.exports =  function(url, method, data, callback){
 	if(method == 'POST'){
-		request.post(url, {'form':data}, function(error,httpResponse,body){
+		request.post(url, {'form':JSON.stringify(data)}, function(error,httpResponse,body){
 			callback(error,httpResponse, body);
 		})
 	}else{
